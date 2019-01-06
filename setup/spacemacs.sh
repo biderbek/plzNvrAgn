c8edcba000183095310947555f69fc6ad66b4309
John Sharpe <john.t.sharpe@gmail.com>
2019-01-06 11:51:38 -0800

    add spacemacs setup - fonts ubuntu

--- setup/spacemacs.sh
+++ setup/spacemacs.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-sudo git clone --depth 1 https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp
+#https://github.com/adobe-fonts/source-code-pro/releases
+sudo mkdir /usr/share/fonts/opentype/scp
+cd /usr/share/fonts/opentype/scp
+sudo wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Italic.ttf
+sudo wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Roman.ttf
 sudo fc-cache -f -v
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
