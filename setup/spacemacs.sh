f2d2b61812a83b7e9557a2cdfd12129f1715e9e7
John Sharpe <john.t.sharpe@gmail.com>
2019-01-21 23:12:03 +0000

    Add unicode font. Add expect to android setup. Mv to .zshrv. update spacemacs.sh

--- setup/spacemacs.sh
+++ setup/spacemacs.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-#https://github.com/adobe-fonts/source-code-pro/releases
-sudo mkdir /usr/share/fonts/opentype/scp
-cd /usr/share/fonts/opentype/scp
-sudo wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Italic.ttf
-sudo wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Roman.ttf
-sudo fc-cache -f -v
+
+if [[ "$(uname)" == "Lin" ]]; then
+    #https://github.com/adobe-fonts/source-code-pro/releases
+    sudo mkdir /usr/share/fonts/opentype/scp
+    cd /usr/share/fonts/opentype/scp
+    sudo wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Italic.ttf
+    sudo wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Roman.ttf
+    sudo fc-cache -f -v
+    sudo apt -y install ispell
+    sudo apt -y install iamerican
+    sudo apt -y install opam python-pip
+    sudo apt -y install fonts-dejavu fonts-noto
+    sudo apt -y install emacs25
+fi
+
+sudo pip install autoflake
+sudo pip install hy
+sudo pip install jedi
+sudo pip install json-rpc
+sudo pip install service_factory
+sudo pip install jupyter
+
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
